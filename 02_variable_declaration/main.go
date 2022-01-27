b6ce92fb46b68ef762a483771e34ac6b44dce801
MrEmin <emuhammet.ozdemir@gmail.com>
2022-01-28 00:00:40 +0300

    Ders 5

---
+++ 02_variable_declaration/main.go
@@ -0,0 +1,20 @@
+// Package clause
+package main
+
+// Import statement
+import "fmt"
+
+// My Code
+func main() {
+
+	//var name string = "Emin" // var - name of variable - static type
+	name := "Emin"
+
+	age := 22
+
+	isMarried := false
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
