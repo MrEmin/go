391a3da86068950517a1d111e14ffa14f996af21
MrEmin <emuhammet.ozdemir@gmail.com>
2022-05-23 14:31:08 +0300

    Ders 31

---
+++ 28_slices_part_II/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	/* underArray := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(underArray)
+
+	mySlc := underArray[2:5] // [2 3 4] , 5. index dahil değil.
+
+	mySlc2 := underArray[:6] //en baştan 6. indexe kadar al.
+	fmt.Println(mySlc2)
+
+	mySlc3 := underArray[3:] // 3. indexten sonuncuya kadar hepsini alır.
+	fmt.Println(mySlc3)
+
+	mySlc4 := underArray[:] // bütün elemanları alır
+	fmt.Println(mySlc4)
+
+	fmt.Println("-----------------------")
+
+	fmt.Println(mySlc)
+	mySlc[0] = 100
+	fmt.Println(mySlc)
+	fmt.Println(mySlc2)
+	fmt.Println(mySlc4) */
+
+}
