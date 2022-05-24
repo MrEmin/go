adcf261762375ecb67dfae7f3704292420b6e729
MrEmin <emuhammet.ozdemir@gmail.com>
2022-05-24 22:58:59 +0300

    Ders 33

---
+++ 30_practice/main.go
@@ -0,0 +1,13 @@
+// 1 -) 5 string elemandan oluşan bir array ve 5 int elemandan oluşan slice oluşturup index numaralarıyla birlikte gösterelim.
+
+package main
+
+func main() {
+
+}
+
+// 2 -) [0,1,2,3,4,5,6,7,8] slice'dan [0,1,2,3], [4,5,6], [6,7,8], [2,3,6,7] slicelarını oluşturunuz.
+
+// 3 -) slicelar için copy metodunu ve assign ( = ) ile farkını açıklayınız.
+
+// 4 -) map gösterimi ile yazar ve yazarlara ait kitapların isimlerini ofr rarne ile gösterin.
